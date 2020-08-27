4f45ad8e803741116a7caeafef192564f6fddfd4
CODEMCD <psbum1609@gmail.com>
2020-08-27 18:40:50 +0900

    init project

---
+++ src/test/java/com/codemcd/springbootmybatistest/SpringBootMybatisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemcd.springbootmybatistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
